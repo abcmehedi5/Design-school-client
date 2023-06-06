f07ebf87ea8c9b91c405c26f742955f40a7421e9
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-06 23:44:49 +0600

    router setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Home = () => {
+  return (
+    <>
+      <h1>mehedi</h1>
+      <h1>mehedi</h1>
+      <h1>mehedi</h1>
+      <h1>mehedi</h1>
+    </>
+  );
+};
+
+export default Home;
