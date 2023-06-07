de23f9f761984561b6f853a8b5cb105a754144b5
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-07 21:18:30 +0600

    integration axios secure

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -11,7 +11,32 @@ const useAxiosSecure = () => {
   const { logOut } = useContext(AuthContext);
   const navigate = useNavigate();
 
-//   useEffect(() => {}, []);
+  useEffect(() => {
+    //  request-----------------------
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    //  response---------------------
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          (error.response && error.response.status === 401) ||
+          error.response.status === 403
+        ) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate]);
 
   return [axiosSecure];
 };
