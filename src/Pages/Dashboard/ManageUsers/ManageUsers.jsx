b604d5cb65d312b2e33346a4bbe918103b586cf7
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-08 10:55:11 +0600

    user role update

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { MdDelete } from "react-icons/md";
+import { useToaster } from "react-hot-toast";
+import Swal from "sweetalert2";
 const ManageUsers = () => {
   const [axiosSecure] = useAxiosSecure();
+  const [role, setRole] = useState("");
+
   const {
     data: users = [],
     refetch,
@@ -13,7 +17,41 @@ const ManageUsers = () => {
     return res.data;
   });
 
-  console.log(users);
+  // handle role update
+  const handleRole = (id, event) => {
+    const data = event.target.value;
+    console.log(id, data);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .put(`/users/${id}`, { data })
+          .then((result) => {
+            refetch();
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${data} has been created`,
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          })
+          .catch((error) => {
+            console.log(error.message);
+            useToaster("success", error);
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <h1 className="text-lg font-bold text-center mt-10 mb-10">
@@ -35,12 +73,27 @@ const ManageUsers = () => {
           {!isLoading ? (
             <tbody className="text-center">
               {users.map((user, index) => (
-                <tr className="bg-slate-100 p-5">
+                <tr key={index} className="bg-slate-100 p-5">
                   <td>{index + 1}</td>
                   <td>{user?.name}</td>
                   <td>{user?.email}</td>
                   <td>{user?.phoneNumber}</td>
-                  <td>{user?.status}</td>
+                  {/* <td><button className="btn btn-sm px-6 bg-green-500">{user?.status}</button></td> */}
+                  <select
+                    className={` ${
+                      (user.status == "admin" && "bg-yellow-500") ||
+                      (user?.status == "instractor" && "bg-success")
+                    } select select-bordered  mt-1`}
+                    onChange={(event) => handleRole(user?._id, event)}
+                  >
+                    <option disabled selected>
+                      {user?.status}
+                    </option>
+                    <option value="admin">Admin</option>
+                    <option value="instractor">Instractor</option>
+                    <option value="user">User</option>
+                  </select>
+
                   <td>
                     <button className="btn btn-sm px-6  bg-red-500">
                       <MdDelete size={20} color="white" />
