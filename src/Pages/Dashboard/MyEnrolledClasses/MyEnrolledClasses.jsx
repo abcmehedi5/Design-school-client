2e2f8904b7d444e240691e3fe52c65f76a17d186
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-10 02:45:05 +0600

    my enrolled class add

--- src/Pages/Dashboard/MyEnrolledClasses/MyEnrolledClasses.jsx
+++ src/Pages/Dashboard/MyEnrolledClasses/MyEnrolledClasses.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useContext } from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "react-query";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import MyEnrolledDetails from "./MyEnrolledDetails";
 
 const MyEnrolledClasses = () => {
-    return (
-        <div>
-            <h1>MyEnrolledClasses</h1>
-        </div>
-    );
+  const [axiosSecure] = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const { data: enrolledClass = [], isLoading } = useQuery({
+    queryKey: ["enrolledClass"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/enroll?email=${user?.email}`);
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      {isLoading ? (
+        <h4 className="text-center">Loading....</h4>
+      ) : (
+        <>
+          {enrolledClass.length <= 0 ? (
+            <h3 className="min-h-screen hero text-5xl">
+              {" "}
+              Sorry !! You are not enrolled in any class
+            </h3>
+          ) : (
+            <div>
+              <h1 className="uppercase text-2xl text-center mt-5 font-bold">
+                My enrolled Class ({enrolledClass.length})
+              </h1>
+
+              <div className="grid lg:grid-cols-2 md: grid-cols-1">
+                {enrolledClass.map((myclasses) => (
+                  <MyEnrolledDetails
+                    myclasses={myclasses}
+                    key={myclasses._id}
+                  ></MyEnrolledDetails>
+                ))}
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MyEnrolledClasses;
---
+++ src/Pages/Dashboard/MyEnrolledClasses/MyEnrolledDetails.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MyEnrolledDetails = ({ myclasses }) => {
+  const { name, image, instructorEmail, instructorName } = myclasses;
+  console.log(myclasses);
+  return (
+    <div className="shadow-md rounded-lg p-4 border mt-3 m-3 bg-slate-100 flex  items-center gap-4">
+      <img
+        src={image}
+        alt="Card Image"
+        className="w-150px] h-[150px] mb-4 rounded-lg border p-1"
+      />
+      <div>
+        <h2 className="text-xl font-bold mb-2">{name}</h2>
+        <p className="text-gray-700 mb-4">
+          Instructor Email: {instructorEmail}
+        </p>
+        <p className="text-gray-700 mb-4">Instructor Name: {instructorName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MyEnrolledDetails;
