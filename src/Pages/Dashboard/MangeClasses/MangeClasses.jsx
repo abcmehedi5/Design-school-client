71ad21650c7d04c069a9535dc27cf09eb712fe43
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-09 11:17:21 +0600

    feedback option enable

--- src/Pages/Dashboard/MangeClasses/MangeClasses.jsx
+++ src/Pages/Dashboard/MangeClasses/MangeClasses.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { AuthContext } from "../../../Providers/AuthProvider";
 import { useQuery } from "react-query";
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdMessage } from "react-icons/md";
 import Swal from "sweetalert2";
 import useToast from "../../../Hooks/useToast";
 
 const MangeClasses = () => {
   const [axiosSecure] = useAxiosSecure();
   const { user } = useContext(AuthContext);
-
+  const [modal, setModal] = useState({});
+  const [message, setMessage] = useState({});
+  const [loading, setLoading] = useState(false);
   const {
     data: classes = [],
     isLoading,
@@ -56,6 +58,34 @@ const MangeClasses = () => {
     });
   };
 
+  const handleMessage = (data) => {
+    setModal(data);
+  };
+
+  const handleSend = (event) => {
+    setLoading(true);
+    event.preventDefault();
+
+    const data = {
+      message: message,
+      instructorEmail: modal.instructorEmail,
+      name: modal.name,
+      image: modal.image,
+      classesId: modal._id,
+    };
+
+    axiosSecure
+      .post("/feedback", data)
+      .then((result) => {
+        useToast("success", "message send successfull");
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1 className="uppercase text-2xl text-center mt-5 font-bold">
@@ -72,6 +102,7 @@ const MangeClasses = () => {
             <th>Available Seats</th>
             <th>Enroll</th>
             <th>Status</th>
+            <th>Message</th>
             <th>Action</th>
           </tr>
         </thead>
@@ -98,7 +129,17 @@ const MangeClasses = () => {
                   <option value="pending">Pending</option>
                   <option value="approved">Approved</option>
                 </select>
-
+                <td>
+                  <button
+                    onClick={() => {
+                      window.my_modal_1.showModal();
+                      handleMessage(cls);
+                    }}
+                    className="btn"
+                  >
+                    <MdMessage size={23} />
+                  </button>
+                </td>
                 <td>
                   <button className="btn btn-sm px-6  bg-red-400 text-white">
                     <MdDelete size={20} color="white" />
@@ -111,6 +152,44 @@ const MangeClasses = () => {
           <span className="loading loading-spinner loading-lg "></span>
         )}
       </table>
+
+      {/* message modal  */}
+
+      {/* Open the modal using ID.showModal() method */}
+      <dialog id="my_modal_1" className="modal relative">
+        <form method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg">{modal.instructorName}</h3>
+          <p className="text-slate-500">{modal.instructorEmail}</p>
+          <div className="py-4">
+            <form>
+              <textarea
+                onChange={(event) => setMessage(event.target.value)}
+                type="text"
+                required={true}
+                placeholder="Type Message"
+                className="input input-bordered w-full h-40"
+              />
+              {/* <input
+                onClick={handleSend}
+                className="btn absolute bottom-5 left-5"
+                type="button"
+                value="send"
+              /> */}
+
+              <button onClick={handleSend} className="btn absolute bottom-5 left-5">
+                {loading && (
+                  <span className="loading loading-spinner loading-md"></span>
+                )}
+                Send
+              </button>
+            </form>
+          </div>
+          <div className="modal-action">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn">Close</button>
+          </div>
+        </form>
+      </dialog>
     </div>
   );
 };
