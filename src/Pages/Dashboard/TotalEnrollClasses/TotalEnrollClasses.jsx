2c7bf6be39bae8ec08679a2119daf2600d67c9e0
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-10 12:23:49 +0600

    total enrolled student

--- src/Pages/Dashboard/TotalEnrollClasses/TotalEnrollClasses.jsx
+++ src/Pages/Dashboard/TotalEnrollClasses/TotalEnrollClasses.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import { useQuery } from "react-query";
 
 const TotalEnrollClasses = () => {
-    return (
-        <div>
-            <h1>total enrolled</h1>
-        </div>
-    );
+  const [axiosSecure] = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const [enrollUser , setUserEnroll] = useState({})
+  const { data: enrollStudent = [], isLoading } = useQuery({
+    queryKey: ["enrollStudent", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/enroll/instructor?email=${user?.email}`
+      );
+      return res.data;
+    },
+  });
+
+  return (
+    <div>
+      <h1>total enrolled</h1>
+    </div>
+  );
 };
 
 export default TotalEnrollClasses;
