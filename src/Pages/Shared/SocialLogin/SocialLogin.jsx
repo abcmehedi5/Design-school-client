527977ec3f5c284cafbfb6f9f4f7a5090fcce813
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-10 16:09:30 +0600

    social login create user

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,14 +1,33 @@
 import React, { useContext } from "react";
 import { FaGofore } from "react-icons/fa";
 import { AuthContext } from "../../../Providers/AuthProvider";
+import useToast from "../../../Hooks/useToast";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const [axiosSecure] = useAxiosSecure();
   const { googleSingin } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
   const handlegoogleSingin = () => {
     googleSingin()
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        const saveUser = {
+          name: loggedInUser.displayName,
+          email: loggedInUser.email,
+          phoneNumber: "",
+          status: "user",
+          image: loggedInUser.photoURL,
+        };
+        console.log(loggedInUser);
+        axiosSecure.post("/users", saveUser).then((result) => {
+          navigate(from, { replace: true });
+          useToast("success", "account create successfull");
+        });
       })
       .catch((error) => {
         const errorMessage = error.message;
